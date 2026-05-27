17e0c608edb8a1393a865fa6d4dc34e73bffc57a
김민조 <mjkim@bridge-abm.com>
2026-05-27 15:35:05 +0900

    Simplify footer contact line

--- quartz/components/Footer.tsx
+++ quartz/components/Footer.tsx
@@ -1,29 +1,25 @@
 import { QuartzComponent, QuartzComponentConstructor, QuartzComponentProps } from "./types"
 import style from "./styles/footer.scss"
-import { version } from "../../package.json"
-import { i18n } from "../i18n"
 
 interface Options {
   links: Record<string, string>
 }
 
 export default ((opts?: Options) => {
-  const Footer: QuartzComponent = ({ displayClass, cfg }: QuartzComponentProps) => {
+  const Footer: QuartzComponent = ({ displayClass }: QuartzComponentProps) => {
     const year = new Date().getFullYear()
-    const links = opts?.links ?? []
+    const email = Object.entries(opts?.links ?? {})[0]
     return (
       <footer class={`${displayClass ?? ""}`}>
         <p>
-          {i18n(cfg.locale).components.footer.createdWith}{" "}
-          <a href="https://quartz.jzhao.xyz/">Quartz v{version}</a> © {year}
+          © {year} 김민조 / All rights reserved
+          {email ? (
+            <>
+              {" / "}
+              <a href={email[1]}>{email[0]}</a>
+            </>
+          ) : null}
         </p>
-        <ul>
-          {Object.entries(links).map(([text, link]) => (
-            <li>
-              <a href={link}>{text}</a>
-            </li>
-          ))}
-        </ul>
       </footer>
     )
   }
